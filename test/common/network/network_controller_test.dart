191e064aeba68883878e4627370facee20f1c649
iveloff-developer <iveloff.work@gmail.com>
2020-12-01 12:31:40 +0200

    Created NetworkController and test file for it.

---
+++ test/common/network/network_controller_test.dart
@@ -0,0 +1,20 @@
+import 'package:data_connection_checker/data_connection_checker.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:netsells_test/common/network/network_controller.dart';
+
+class MockDataConnectionChecker extends Mock implements DataConnectionChecker {}
+
+void main() {
+  MockDataConnectionChecker mockDataConnectionChecker;
+  NetworkController networkController;
+
+  setUp(
+    () {
+      mockDataConnectionChecker = MockDataConnectionChecker();
+      networkController = NetworkControllerImpl(
+        dataConnectionChecker: mockDataConnectionChecker,
+      );
+    },
+  );
+}
