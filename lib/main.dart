b849f25bca45d135d8822f8e13da5ef7fb800a73
iveloff-developer <iveloff.work@gmail.com>
2020-12-01 18:25:01 +0200

    Setted up injection.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:netsells_test/common/injection/injection.dart';
 
 import 'app.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  setupInjections();
   runApp(App());
 }
