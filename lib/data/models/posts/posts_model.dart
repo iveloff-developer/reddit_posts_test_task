b0fddf36e13618e07514015b4945591622e65888
iveloff-developer <iveloff.work@gmail.com>
2020-12-01 18:55:15 +0200

    Updated Posts and related classes.

--- lib/data/models/posts/posts_model.dart
+++ lib/data/models/posts/posts_model.dart
@@ -3,20 +3,10 @@ import 'package:netsells_test/data/models/posts/post_model.dart';
 import 'package:netsells_test/domain/entities/posts/posts.dart';
 
 class PostsModel extends Posts {
-  PostsModel({
-    @required String modhash,
-    @required int dist,
-    @required List<PostModel> children,
-  }) : super(
-          modhash: modhash,
-          dist: dist,
-          children: children,
-        );
+  PostsModel({@required List<PostModel> children}) : super(children: children);
 
   factory PostsModel.fromJson(Map<String, dynamic> json) {
     return PostsModel(
-      modhash: json[PostsModelKeys.modhash.key],
-      dist: json[PostsModelKeys.dist.key],
       children: List.from(json[PostsModelKeys.children.key])
           .map((postJsonMap) => PostModel.fromJson(postJsonMap))
           .toList(),
@@ -24,11 +14,7 @@ class PostsModel extends Posts {
   }
 }
 
-enum PostsModelKeys {
-  modhash,
-  dist,
-  children,
-}
+enum PostsModelKeys { children }
 
 extension on PostsModelKeys {
   String get key => this.toString().split('.').last;
--- lib/domain/entities/posts/posts.dart
+++ lib/domain/entities/posts/posts.dart
@@ -3,20 +3,10 @@ import 'package:netsells_test/domain/entities/posts/post.dart';
 import 'package:meta/meta.dart';
 
 class Posts extends Equatable {
-  final String modhash;
-  final int dist;
   final List<Post> children;
 
-  Posts({
-    @required this.modhash,
-    @required this.dist,
-    @required this.children,
-  });
+  Posts({@required this.children});
 
   @override
-  List<Object> get props => [
-        modhash,
-        dist,
-        children,
-      ];
+  List<Object> get props => [children];
 }
