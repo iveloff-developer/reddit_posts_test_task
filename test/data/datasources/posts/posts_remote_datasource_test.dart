8f238beeda2ce7aedd227ff7a966c363d21b3897
iveloff-developer <iveloff.work@gmail.com>
2020-12-01 17:33:09 +0200

    Refactored test.

--- test/data/datasources/posts/posts_remote_datasource_test.dart
+++ test/data/datasources/posts/posts_remote_datasource_test.dart
@@ -5,8 +5,6 @@ import 'package:netsells_test/common/exceptions/server_exception.dart';
 import 'package:netsells_test/common/network/rest_endpoints.dart';
 import 'package:netsells_test/common/network/rest_headers.dart';
 import 'package:netsells_test/data/datasources/posts/posts_remote_datasource.dart';
-import 'package:netsells_test/data/models/posts/post_model.dart';
-import 'package:netsells_test/data/models/posts/posts_model.dart';
 import 'package:netsells_test/domain/credentials/posts/posts_type_credential.dart';
 
 import '../../../fixtures/json_files.dart';
@@ -27,49 +25,61 @@ void main() {
 
   final postsModel = TestData.postsModel;
 
-  test(
-    'should preform a GET request on a URL with posts type being the endpoint and with application/json header',
+  group(
+    'request successful',
     () {
-      when(mockClient.get(any, headers: anyNamed('headers'))).thenAnswer(
-        (_) async => Response(JsonFiles.listing.fixture(), 200),
+      setUp(
+        () {
+          when(mockClient.get(any, headers: anyNamed('headers'))).thenAnswer(
+            (_) async => Response(JsonFiles.listing.fixture(), 200),
+          );
+        },
+      );
+      test(
+        'should preform a GET request on a URL with posts type being the endpoint and with application/json header',
+        () {
+          rds.getPosts(PostsTypeCredential.Hot);
+
+          verify(
+            mockClient.get(
+              RestEndpoints.FlutterDev.url(PostsTypeCredential.Hot),
+              headers: {
+                RestHeadersKeys.contentType: RestHeadersValues.applicationJson,
+              },
+            ),
+          );
+        },
       );
 
-      rds.getPosts(PostsTypeCredential.Hot);
+      test(
+        'should return [Posts]',
+        () async {
+          final result = await rds.getPosts(PostsTypeCredential.Hot);
 
-      verify(
-        mockClient.get(
-          RestEndpoints.FlutterDev.url(PostsTypeCredential.Hot),
-          headers: {
-            RestHeadersKeys.contentType: RestHeadersValues.applicationJson,
-          },
-        ),
+          expect(result, equals(postsModel));
+        },
       );
     },
   );
 
-  test(
-    'should return [Posts] when the response code is 200 (success)or other',
-    () async {
-      when(mockClient.get(any, headers: anyNamed('headers'))).thenAnswer(
-        (_) async => Response(JsonFiles.listing.fixture(), 200),
+  group(
+    'request failed',
+    () {
+      setUp(
+        () {
+          when(mockClient.get(any, headers: anyNamed('headers'))).thenAnswer(
+            (_) async => Response('Something went wrong', 404),
+          );
+        },
       );
-
-      final result = await rds.getPosts(PostsTypeCredential.Hot);
-
-      expect(result, equals(postsModel));
-    },
-  );
-
-  test(
-    'should throw [ServerException] when the response code is 404 or other',
-    () async {
-      when(mockClient.get(any, headers: anyNamed('headers'))).thenAnswer(
-        (_) async => Response('Something went wrong', 404),
-      );
-
-      expect(
-        rds.getPosts(PostsTypeCredential.Hot),
-        throwsA(isInstanceOf<ServerException>()),
+      test(
+        'should throw [ServerException] when the response code is 404 or other',
+        () async {
+          expect(
+            rds.getPosts(PostsTypeCredential.Hot),
+            throwsA(isInstanceOf<ServerException>()),
+          );
+        },
       );
     },
   );
